f34188b152eea9de974e267f1614971bdbff5795
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-05-04 18:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,26 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+async function fetchData(apiUrls){
+	const starttimeAll=Date.now();
+	const allPromise=apiUrls.map(url=>
+		fetch(url));
+	await Promise.all(allPromise);
+	const endtimeAll=Date.now();
+	const timetakenAll=endtimeAll-starttimeAll;
+
+	const starttimeAny=Date.now();
+	const anyPromise=apiUrls.map(url=>
+		fetch(url));
+	await Promise.any(anyPromise);
+	const endtimeAny=Date.now();
+	const totaltakenAny=endtimeAny-starttimeAny;
+
+	const results=document.getElementbyId('output-all').innerHTML=timetakenAll;
+	const result2=document.getElementbyId('output-any').innerHTML=totaltakenAny;
+	
+}
+
+
 
 // You can write your code here
